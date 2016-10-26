815bb8b6f37f62e5e8818b5dc8e2e88a93c2daf5
Haseeb <talhamahmood9@gmail.com>
2016-10-26 14:37:47 +0500

    DOne

--- app/src/main/java/com/example/jnetbackup/swipe/WebService.java
+++ app/src/main/java/com/example/jnetbackup/swipe/WebService.java
@@ -17,9 +17,11 @@ public class WebService {
     //Namespace of the Webservice - It is http://tempuri.org for .NET webservice
     private final static String NAMESPACE = "http://jms.hopto.org:806";
     //private final static String URL = "https://jms.hopto.org:805/JMS_Auth_WebService.asmx";
-    private final static String URL =  "https://59.103.165.45:3393/JMS_Auth_WebService.asmx";
+    final static String URL ="https://198.38.93.76:806/JMS_Auth_WebService.asmx";
+    //private final static String URL =  "https://59.103.165.45:3393/JMS_Auth_WebService.asmx";
     private final static String SOAP_ACTION = "http://jms.hopto.org:806/GetListOfDevicesInBranch";
     private final static String SOAP_ACTION1 = "http://jms.hopto.org:806/GetUpdatedDataInDevice";
+    private final static String SOAP_ACTION2 = "http://jms.hopto.org:806/GetThresholdOfDevice";
     private final static String METHOD_NAME = "AuthenticateUser";
     public static SoapObject invokeHelloWorldWS(String username,long name, String Pass, String s) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         SoapObject resTxt = null;
@@ -71,6 +73,60 @@ response.getPropertyCount();
 
             //Assign error message to resTxt
           //  resTxt = "Error occured";
+        }
+        //Return resTxt to calling object
+        return resTxt;
+    }
+    public static SoapObject getthreshold(String username,long name, String Pass, String s) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        SoapObject resTxt = null;
+        // Create request
+        SoapObject request = new SoapObject(NAMESPACE,"GetThresholdOfDevice");
+
+        // Property which holds input parameters
+        PropertyInfo Branch_id = new PropertyInfo();
+        Branch_id.setName("Device_id");
+        Branch_id.setValue(name);
+        Branch_id.setType(long.class);
+        request.addProperty(Branch_id);
+        PropertyInfo Token = new PropertyInfo();
+        Token.setName("Token");
+        Token.setValue(Pass);
+        Token.setType(String.class);
+        request.addProperty(Token);
+        PropertyInfo user = new PropertyInfo();
+        user.setName("userName");
+        user.setValue(username);
+        user.setType(String.class);
+        request.addProperty(user);
+
+
+        // Create envelope
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+                SoapEnvelope.VER11);
+        //Set envelope as dotNet
+        envelope.dotNet = true;
+        // Set output SOAP object
+        envelope.setOutputSoapObject(request);
+        // Create HTTP call object
+        SSlconnection.allowAllSSL();
+        HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+
+        try {
+            // Invoke web service
+            androidHttpTransport.call(SOAP_ACTION2, envelope);
+            // Get the response
+            SoapObject response=(SoapObject)envelope.getResponse();
+            // Assign it to resTxt variable static variable
+            resTxt = response;
+            response.getPropertyCount();
+            //  Log.d("response",response.getPropertyAsString(0)+""+response.getPropertyCount());
+
+        } catch (Exception e) {
+            //Print error
+            e.printStackTrace();
+
+            //Assign error message to resTxt
+            //  resTxt = "Error occured";
         }
         //Return resTxt to calling object
         return resTxt;
